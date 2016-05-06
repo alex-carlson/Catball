356ff42d8d37fac94d9dd1d7995cd31071c9a54c
Alex Carlson <alex@acwd.me>
2016-05-06 15:19:27 -0500

    prototype level & placeholder nip

---
+++ Assets/Scripts/levelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelManager : MonoBehaviour {
+
+	public static int playerCount;
+	GameObject levelGeometry;
+
+	// Use this for initialization
+	void Start () {
+		levelGeometry = GameObject.Find ("Level");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator levelGen(){
+		
+		yield return new WaitForSeconds (1);
+	}
+}

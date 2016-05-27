9834b874077b6606ffc44336003bc11d75012d7b
Alex Carlson <alex@acwd.me>
2016-05-27 16:15:18 -0500

    additive scene loading and player count at menu

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -4,10 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
+	public GameObject mainMenuItems;
+
 
     public void LoadLevel()
     {
 		
-        SceneManager.LoadScene(1);
+		SceneManager.LoadSceneAsync ("Hill", LoadSceneMode.Additive);
+		fadeOut ();
     }
+
+	public void fadeOut(){
+		mainMenuItems.SetActive (false);
+		//this.gameObject.SetActive (false);
+	}
 }
